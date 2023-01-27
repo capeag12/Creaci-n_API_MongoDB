9026022bd2a5555e9c7b4a7a35454ae679ce4fa6
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-01-27 10:42:53 +0100

    V1

--- app.js
+++ app.js
@@ -69,7 +69,36 @@ app.get('/buscarcontacto',(req,res)=>{
 })
 
 app.put('/actualizarcontacto',(req,res)=>{
-    
+    let objectId = req.query.id
+    if (!objectId) {
+        return res.send("Error")
+    }
+    console.log(req.body)
+    if (req.body) {
+        try{
+            let obID = mongodb.ObjectId(objectId)
+            let recibido = db.collection('contactos').updateOne({"_id":obID},{ $set: req.body }).then((result) => {
+                console.log(result)
+                if (result.modifiedCount>0) {
+                    return res.send("Se han modificado"+result.modifiedCount+" objetos")
+                }
+                
+                
+            }).catch((err) => {
+                return res.send('Ha fallado')
+            }); 
+            
+        }
+        catch{
+            return res.send("Error, el objectId no tiene el formato correcto")
+        }
+    }
+    else{
+        return res.send("Debes enviar un body")
+        
+        
+        
+    }
 })
 
 app.delete('/eliminarcontacto', (req,res)=>{
